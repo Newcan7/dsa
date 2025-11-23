f7cdc6e352850d2a8fb8e9aac0d263cf0a1e22ab
Newcan <ppsspp.me@gmail.com>
2025-11-23 23:52:06 +0530

    23 nov

--- circular_queue.c
+++ circular_queue.c
@@ -1,52 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX 5
-int rear =-1,front=0,count=0;
-void insert(int a[],int x);
-int delete(int a[]);
+int rear = -1, front = -1;
+int queue[MAX];
+void insert(int x);
+int delete();
+void display();
 int main()
 {
-	int a[MAX],n,m,ch;
-	while(1)
-	{
-		printf("1: Insert \n");
-		printf("2: Delete \n");
-		printf("Enter choice : \n");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			case 1: printf("Enter element : \n");
-					scanf("%d",&n);
-					insert(a,n);
-					break;
-			case 2: m = delete(a);
-			        printf("deleted element is : %d \n",m);	
-					break;
-			default : exit(1);			
-		}
-	}
-	return 0;		
+    int n,ch;
+    while (1)
+    {
+        printf("1: Insert \n");
+        printf("2: Delete \n");
+        printf("3. Display \n");
+        printf("Enter choice : \n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter element : ");
+            scanf("%d", &n);
+            insert(n);
+            break;
+        case 2: 
+            printf("Deleted element is : %d\n",delete());
+            break;
+        case 3:
+            display();
+            break;
+        }
+    }
+    return 0;
 }
-void insert(int a[],int x)
+void insert(int x)
 {
-	if(count>=MAX)
-		printf("Queue Full \n");
-	else
-	count++;
-	if(rear>=MAX)
-	rear=0;
-	else
-	rear ++;
-	
-	a[rear]=x;
+    if (front == (rear + 1) % MAX)
+    {
+        printf("Queue Full\n");
+        return;
+    }
+    if (front == -1)
+        front = 0;
+    rear = (rear + 1) % MAX;
+    queue[rear] = x;
+    printf("%d inserted \n", x);
 }
-int delete(int a[])
+int delete()
 {
-	int x;
-	if(count<=0)
-	printf("Queue Empty \n");
-	else 
-	count--;
-	x=a[front];
-	return x;
+    int x;
+    if (front == -1)
+    {
+        printf("Queue Empty\n");
+        return -1;
+    }
+    x = queue[front];
+    if (front == rear) 
+        front = rear = -1;
+    else
+        front = (front + 1) % MAX;
+    return x;
+}
+void display()
+{
+    int i;
+    for(i=front;i<=rear;i=(i+1)%MAX)
+    {
+        printf("%d<-- ", queue[i]);
+    }
+    printf("\n");
 }
